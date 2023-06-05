51c3d205af6231899cab302acec1c8eb66919768
r164g198b57 <example@example.com>
2023-06-05 16:31:44 +0300

    Refresh dependencies, activity_main.xml

---
+++ app/src/main/java/com/github/r164g198b57/geoquiz/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.github.r164g198b57.geoquiz
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
